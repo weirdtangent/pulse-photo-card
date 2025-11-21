ae7b6b7836113cdab33f4db0dd22ae4a56200d6a
Jeff Culverhouse <jeff@graystorm.com>
2025-11-21 13:32:00 -0500

    chore: actually, use default release rules

--- release.config.js
+++ release.config.js
@@ -1,27 +1,7 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "angular",
-        releaseRules: [
-          { type: "feat", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-          { type: "revert", release: "patch" },
-          { type: "chore", release: "patch" },
-          { type: "build", release: "patch" },
-          { type: "ci", release: "patch" },
-          { type: "docs", release: "patch" },
-          { type: "refactor", release: "patch" },
-          { type: "style", release: "patch" },
-          { type: "test", release: "patch" },
-          { breaking: true, release: "major" },
-          { type: "*", release: "patch" }, // fallback for non-conventional commits
-        ],
-      },
-    ],
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
